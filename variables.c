529398003ed42cd46d6339a83b1a9f27f44211b8
Emre Dayangaç <120194760+EmreDay1@users.noreply.github.com>
2025-05-17 01:03:43 +0300

    var's

---
+++ variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+
+   int x; //declaration
+   x = 14; // init
+   int y = 13; // combined
+   float ne = 2.05;
+   char grade = 'A';
+   char name[] = "Bro"; //array
+   
+   printf("You are %d years old", x); 
+
+   return 0;
+
+}
